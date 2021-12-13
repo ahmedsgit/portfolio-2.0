9a86bdc3d8b010c640e9b1429bb466754c18aefc
ahmedsgit <acc.ahmedsazzad@gmail.com>
2021-12-13 12:35:48 +0600

    Project setup

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+    arrowParens: 'avoid',
+    singleQuote: true,
+    bracketrSpacing: true,
+    endOfLine: 'lf',
+    semi: false,
+    tabWidth:2,
+    trialingComma: 'none'
+}
+
+module.exports = options
