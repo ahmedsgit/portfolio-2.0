d975b06ea80e128f398e00f2c20da36e0beb1b90
ahmedsgit <acc.ahmedsazzad@gmail.com>
2021-12-15 11:47:24 +0600

    Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
+import { Container,Box,Heading } from "@chakra-ui/react";
 const page = ()=>{
     return (
-        <div>Hello</div>
+        <Container>
+            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+                Hello I&apos;m a full-stack Web developer based in Bangladesh!
+            </Box>
+            <Box display={{md: 'flex'}}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Ahmed Sazzad
+                    </Heading>
+                    <p>Web Lover (UI/UX Designer | Developer | Designer) </p>
+                </Box>
+
+            </Box>
+        </Container>
     )
 }
 
